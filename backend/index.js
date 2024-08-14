1ee309ed97f868eb97b694c6cef7de28311a8d77
Hilal Nur DURDAÇ <durdachilalnur@gmail.com>
2024-08-14 20:03:43 +0300

    future[]
    mongoDB err

--- backend/index.js
+++ backend/index.js
@@ -11,13 +11,15 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Bağlantısı
-mongoose.connect('mongodb://localhost:27017/mywebsite', {
+mongoose.connect('mongodb://localhost:27017/site', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
   .then(() => console.log('MongoDB bağlantısı başarılı'))
   .catch((err) => console.error('MongoDB bağlantı hatası:', err));
 
+
+  
 // API Routes
 app.use('/api', apiRoutes);
 
