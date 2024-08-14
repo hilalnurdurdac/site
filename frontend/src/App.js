f9ae0b7c2252d77b8c037f2aea4349dd5cbd2c6e
Hilal Nur DURDAÇ <durdachilalnur@gmail.com>
2024-08-14 18:37:35 +0300

    future[]
    frontend add app,src,index

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My Website</h1>
+      <p>This is the home page.</p>
+    </div>
+  );
+}
+
+export default App;
+
